f3b2f863c959061f9975c5575dbd444e3d58369f
Angad SIngh <angadsingh567890@gmail.com>
2018-06-12 02:53:49 +0530

    Finish file watcher

---
+++ watch.js
@@ -0,0 +1,34 @@
+const chokidar = require('chokidar');
+const { spawn } = require('child_process');
+
+const srcWatcher = chokidar.watch('src/');
+const postsWatcher = chokidar.watch('posts/');
+
+srcWatcher
+  .on('add', runBuild)
+  .on('change', runBuild)
+  .on('unlink', runBuild)
+  .on('error', (error) => console.error('Error: ', error));
+postsWatcher
+  .on('add', runBuild)
+  .on('change', runBuild)
+  .on('unlink', runBuild)
+  .on('error', (error) => console.error('Error: ', error));
+
+function reload() {
+	const code  = 'tell application "Google Chrome"\n\
+  reload active tab of window 1\n\
+end tell\n';
+	spawn('osascript', ['-e', code])
+		.on('close', () => console.log('Browser reloaded'))
+		.on('error', (e) => console.error(e));
+}
+
+function runBuild() {
+	spawn('node', ['index'])
+		.on('close', () => {
+			console.log('Finished build');
+			reload();
+		})
+		.on('error', (e) => console.error(e));
+}
