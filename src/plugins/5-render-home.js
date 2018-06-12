0a7fddf0f64c97069ee4a65025ff694e5363a9b0
Angad Singh <angadsingh567890@gmail.com>
2018-06-13 00:01:05 +0530

    Async-await rewrite for the render-home plugin

--- src/plugins/5-render-home.js
+++ src/plugins/5-render-home.js
@@ -9,20 +9,18 @@ const path = require('path');
  * @param {array} posts An array of post objects.
  * @param {object} c User config
  */
-function renderHome(posts, c) {
-	return new Promise(async function (resolve, reject) {
-		try {
-			let displayedPosts = posts.filter(post => {
-				return post.hasOwnProperty('display') ? post.display : true;
-			});
-
-			const html = await promisify(ejs.renderFile)(path.join(c.templates, 'home.ejs'), { title: 'Angad Singh\'s Blog', posts: displayedPosts});
-			await promisify(fs.writeFile)(path.join(c.build, 'index.html'), html);
-
-			console.log('Rendered homepage');
-			resolve(posts);
-		} catch(e) { reject(e) }
-	});
+async function renderHome(posts, c) {
+	try {
+		let displayedPosts = posts.filter(post => {
+			return post.hasOwnProperty('display') ? post.display : true;
+		});
+		
+		const html = await promisify(ejs.renderFile)(path.join(c.templates, 'home.ejs'), { title: 'Angad Singh\'s Blog', posts: displayedPosts});
+		await promisify(fs.writeFile)(path.join(c.build, 'index.html'), html);
+		
+		console.log('Rendered homepage');
+		return posts;
+	} catch(e) { return Promise.reject(e); }
 }
 
 module.exports = renderHome;
