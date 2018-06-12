647e4ea0c966972435973bda4299675ea9f18e27
Angad Singh <angadsingh567890@gmail.com>
2018-06-12 20:24:21 +0530

    Async-await rewrite for the clean plugin

--- src/plugins/1-clean.js
+++ src/plugins/1-clean.js
@@ -10,26 +10,19 @@ const path = require('path');
  * @param {array} posts An array of post objects.
  * @param {object} c User config
  */
-function clean(posts, c) {
-	return new Promise((resolve, reject) => {
+async function clean(posts, c) {
+	try {
 		// Delete build directory
-		trash(c.build)
-			.then(() => {
-				console.log('Build directory deleted.');
-				// Create new public directory
-				return promisify(fs.mkdir)(c.build);
-			})
-			.then(() => {
-				console.log('New build directory created');
-				// Copy assets directory
-				return ncp(c.assets, path.join(c.build, 'assets'));
-			})
-			.then(() => {
-				console.log('Assets directory copied')
-				resolve(posts);
-			})
-			.catch(e => reject(e))
-	});
+		await trash(c.build);
+		console.log('Build directory deleted.');
+		// Create new public directory
+		await promisify(fs.mkdir)(c.build);
+		console.log('New build directory created');
+		// Copy assets directory
+		await ncp(c.assets, path.join(c.build, 'assets'));
+		console.log('Assets directory copied')
+		return posts;
+	} catch(e) { return Promise.reject(e) }
 }
 
 module.exports = clean;
