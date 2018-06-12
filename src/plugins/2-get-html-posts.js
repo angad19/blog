b99267b2655b8924f2c0c54ea119b6e0c5ed46ed
Angad Singh <angadsingh567890@gmail.com>
2018-06-12 21:31:22 +0530

    Async-await rewrite for the get-html-posts plugin

--- src/plugins/2-get-html-posts.js
+++ src/plugins/2-get-html-posts.js
@@ -10,31 +10,25 @@ const Post = require('../Post');
  * @param {array} posts An array of post objects.
  * @param {object} c User config
  */
-function getHTMLPosts(posts, c) {
-	return new Promise(async function (resolve, reject) {
-		try {
-			let pList = await promisify(fs.readdir)(c.posts);
-			pList = pList.filter(p => {
-				return path.extname(p) == '.html' || path.extname(p) == '.html';
-			})
-
-			for(let pPath of pList) {
-				// Read file
-				const raw = await promisify(fs.readFile)(path.join(c.posts, pPath), 'utf-8');
-				// Parse front matter
-				const fm = front(raw);
-
-				// Generate post object and push to posts array
-				posts.push(new Post(fm));
-			}
-			
-			// Sort posts
-			posts = posts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
-			
-			console.log('Retrieved HTML posts');
-			resolve(posts);
-		} catch(e) { reject(e) }
-	});
+async function getHTMLPosts(posts, c) {
+	try {
+		let pList = await promisify(fs.readdir)(c.posts);
+		pList = pList.filter(p => {
+			return path.extname(p) == '.html' || path.extname(p) == '.html';
+		})
+		for(let pPath of pList) {
+			// Read file
+			const raw = await promisify(fs.readFile)(path.join(c.posts, pPath), 'utf-8');
+			// Parse front matter
+			const fm = front(raw);	
+			// Generate post object and push to posts array
+			posts.push(new Post(fm));
+		}
+		// Sort posts
+		posts = posts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+		console.log('Retrieved HTML posts');
+		return posts;
+	} catch(e) { return Promise.reject(e) }
 }
 
 module.exports = getHTMLPosts;
