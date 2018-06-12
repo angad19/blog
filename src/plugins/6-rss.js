aee60c65b7038b87788939aa3e7c110f803ea366
Angad Singh <angadsingh567890@gmail.com>
2018-06-13 00:02:33 +0530

    Async-await rewrite for rss plugin

--- src/plugins/6-rss.js
+++ src/plugins/6-rss.js
@@ -15,25 +15,23 @@ const feed = new RSS({
  * @param {array} posts An array of post objects.
  * @param {object} c User config
  */
-function RSSfeed(posts, c) {
-	return new Promise(async function (resolve, reject) {
-		try {
-			for(let post of posts) {
-				feed.item({
-					title: post.title,
-					description: post.content.slice(0, 50),
-					guid: new Date(post.date).getTime(),
-					date: post.date
-				});
-			}
-
-			const xml = feed.xml({ indent: true });
-			await promisify(fs.writeFile)(path.join(c.build, 'feed.xml'), xml);
-			
-			console.log('Generated RSS feed');
-			resolve(posts);
-		} catch(e) { reject(e) }
-	});
+async function RSSfeed(posts, c) {
+	try {
+		for(let post of posts) {
+			feed.item({
+				title: post.title,
+				description: post.content.slice(0, 50),
+				guid: new Date(post.date).getTime(),
+				date: post.date
+			});
+		}
+		
+		const xml = feed.xml({ indent: true });
+		await promisify(fs.writeFile)(path.join(c.build, 'feed.xml'), xml);
+		
+		console.log('Generated RSS feed');
+		return posts;
+	} catch(e) { return Promise.reject(e); }
 }
 
 module.exports = RSSfeed;
